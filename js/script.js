4e55147e4ad44396b6fb137766b52fbc2d61fcf9
Pratyush Mohapatra <Ativerc@users.noreply.github.com>
2023-02-17 16:18:28 +0530

    feat: Settings logic in place.

--- js/script.js
+++ js/script.js
@@ -67,10 +67,10 @@ function searchFunction() {
     };
 };
 
-document.addEventListener("keypress", setFocus);
-function setFocus(){
-    document.getElementById("chanSearch").focus();
-}
+// document.addEventListener("keypress", setFocus);
+// function setFocus(){
+//     document.getElementById("chanSearch").focus();
+// }
 
 function isSearchBoxEmpty() {
     let searchBox = document.getElementById('chanSearch');
@@ -92,3 +92,138 @@ function clearListHeader() {
 function emboldenCharacters(searchBoxText, itemText) {
     
 }
+
+// Settigs Events
+
+document.getElementById("settings-reset").addEventListener("click", resetLocalStorage);
+document.getElementById("settings-save-exit").addEventListener("click", onSettingsSave);
+
+// localStorage Stuff
+
+
+
+function onPageload() {
+    if (storageAvailable("localStorage") !== true) {
+        console.log("ModalMessage: Error! localStorage not available!");
+    } else {
+        if (doesLocalStorageHaveData() === true) {
+            getFromLocalStorage();
+        } else {
+            console.log("ModalMessage: localStorage doesn't have data!");
+        }
+    }
+}
+
+function onSettingsSave() {
+    if (storageAvailable("localStorage") !== true) {
+        console.log("ModalMessage: Error! localStorage not available!"); 
+    } else {
+        if (isServerSettingsIncomplete() !== true) {
+            saveInLocalStorage();
+        }   else {
+            console.log("Modal Message: Server Settings incomplete! Please complete all fields!")
+        }
+        
+    }
+}
+
+// Local Storage Handler Functions
+
+function storageAvailable(type) {
+    let storage;
+    try {
+        storage = window[type];
+        const x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch (e) {
+        return e instanceof DOMException && (
+            // everything except Firefox
+            // e.code === 22 ||
+            // Firefox
+            // e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+}
+
+
+function doesLocalStorageHaveData() {
+    if (localStorage.getItem('server')) {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+    // if (storageAvailable('localStorage')) {
+    //     console.log('localStorage Available!');
+    //     if (!localStorage.getItem('server')) {
+    //         saveInLocalStorage();
+    //     } else {
+    //         getFromLocalStorage();
+    //     }
+        
+    // } else {
+    //     // msgModal : localStorage unavailable
+
+    // }
+
+
+function saveInLocalStorage() {
+    localStorage.setItem('server', document.getElementById('serverIPURL').value);
+    localStorage.setItem('topic', document.getElementById('topicText').value);
+
+    console.log("Saved Settings Data in localStorage!");
+    console.log(localStorage);
+}
+
+function getFromLocalStorage() {
+    // Get from local storage
+    const server = localStorage.getItem('server');
+    const topic = localStorage.getItem('topic');
+
+    // Set text input value to derived values
+    document.getElementById('serverIPURL').value = server;
+    document.getElementById('topicText').value = topic;
+
+    // 
+}
+
+function resetLocalStorage() {
+    // Clear local storage
+    localStorage.clear();
+
+    // Set text input value to ""
+    document.getElementById('serverIPURL').value = document.getElementById('topicText').value = "";
+
+    // TODO modalMessage: Cleared!
+    console.log("ModalMessage: localStorage cleared of ALL data!");
+}
+
+function isServerSettingsIncomplete() {
+    const server = document.getElementById("serverIPURL").value
+    const topic = document.getElementById('topicText').value
+
+    if (server === "" || topic === "") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// MQTT
+
+// 
+
+// Diagnostics
+
+function serverPing() {}
